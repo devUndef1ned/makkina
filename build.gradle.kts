9cf75938d53c2740914f00bf271fd07eb3112959
devUndef1ned <oleg.kuzmin@ah.nl>
2024-03-24 21:50:39 +0100

    Setup publishing to Maven central portal

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
+import com.vanniktech.maven.publish.KotlinMultiplatform
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     kotlin("multiplatform") version "1.9.23"
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 group = "io.github.devundef1ned"
-version = "0.01-SNAPSHOT"
+version = "0.01-alpha01"
 
 repositories {
     mavenCentral()
@@ -28,7 +32,7 @@ kotlin {
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
 
-    
+
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -44,5 +48,40 @@ kotlin {
         val jvmTest by getting
         val nativeMain by getting
         val nativeTest by getting
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+    coordinates(group.toString(), name, version.toString())
+
+    pom {
+        name.set("Makkina")
+        description.set("A simple and lightweight Kotlin Multiplatform Finite State Machine library.")
+        url.set("https://github.com/devUndef1ned/makkina")
+
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("devUndef1ned")
+                name.set("Oleg Kuzmin aka devUndef1ned")
+                email.set("ogkuzmin@gmail.com")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/devUndef1ned/makkina")
+            connection.set("scm:git:git//github.com/devUndef1ned/makkina.git")
+            developerConnection.set("scm:git:ssh//git@github.com:devUndef1ned/makkina.git")
+        }
     }
 }
