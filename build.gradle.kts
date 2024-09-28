e5aa1826b90d7c81def836019856f932c1c63060
Oleg Kuzmin <ogkuzmin@gmail.com>
2024-09-28 10:16:52 +0200

    Use KMP plugin 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
-import com.vanniktech.maven.publish.KotlinMultiplatform
 import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
-    kotlin("multiplatform") version "1.9.23"
+    kotlin("multiplatform") version "2.0.20"
     id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
@@ -16,22 +15,7 @@ repositories {
 val coroutineVersions = "1.6.4"
 
 kotlin {
-    jvm {
-        jvmToolchain(19)
-        withJava()
-        testRuns["test"].executionTask.configure {
-            useJUnitPlatform()
-        }
-    }
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
-
+    jvm()
 
     sourceSets {
         val commonMain by getting {
@@ -45,9 +29,6 @@ kotlin {
             }
         }
         val jvmMain by getting
-        val jvmTest by getting
-        val nativeMain by getting
-        val nativeTest by getting
     }
 }
 
